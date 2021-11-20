cb6dda5b961bf3d83def1af8d65ac7eeca8ddf8d
bindi-v <bindi.vaghela@gmail.com>
2021-11-20 17:07:04 -0500

    fixing deletebook fun

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -37,9 +37,11 @@ const SavedBooks = () => {
         throw new Error('something went wrong!');
       }
 
-      const updatedUser = await response.json();
+      //const updatedUser = await response.json();
+     // setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
+      refetch();
     } catch (err) {
       console.error(err);
     }
