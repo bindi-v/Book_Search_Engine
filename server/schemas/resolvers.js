d1c39453aed566947eff7daa7f167a83e3b68ed9
bindi-v <bindi.vaghela@gmail.com>
2021-11-21 18:54:42 -0500

    debugging serve side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,7 +14,7 @@ const resolvers = {
     // },
     me: async (parent, args, context) => {
         if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+            const userData = await User.findOne({ _id: context?.user?._id }).select('-__v -password');
             return userData;
           }
           throw new AuthenticationError('You need to be logged in!');
@@ -48,7 +48,7 @@ const resolvers = {
       const updatedUserBooks = await User.findByIdAndUpdate(
         { _id: context.user._id },
         { $addToSet: { savedBooks: book } },
-        { new: true }
+        { new: true, runValidators: true }
       );
       return updatedUserBooks;
     }
--- server/server.js
+++ server/server.js
@@ -12,14 +12,13 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+server.applyMiddleware({ app });
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
